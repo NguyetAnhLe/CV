f2d250476b258e31d884830e2a94124968f77337
NguyetAnhLe <55035519+NguyetAnhLe@users.noreply.github.com>
2019-09-29 21:55:57 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
-$("h2.my-project").click(function(){
-  $("div.skill-row").slideToggle();
-});
+
+  $("table.snake-table").slideUp();
+
 
 $("h4.snake-project").click(function(){
   $("table.snake-table").slideToggle();
